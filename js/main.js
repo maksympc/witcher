638a3ad7a41bf8cfa1aaf97051aa117b9fa392b4
Maksym Pinchuk <maksympc@gmail.com>
2020-02-07 02:05:32 +0200

    Implement adaptive design

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 var mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
+    slidesPerView: 1,
     loop: true,
     navigation: {
         nextEl: '.arrow'
+    },
+    breakpoints: {
+        541: {
+            slidesPerView: 2
+        }
     }
 });
 
@@ -11,7 +16,15 @@ var mySwiper = new Swiper('.swiper-container', {
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.background = `linear-gradient(180deg, rgba(20, 18, 24, 0.5) 0%, #2D2D2D 100%), url('img/series/series${slides[i].className.slice(-1)}.png')`;
         slides[i].style['background-repeat'] = 'no-repeat';
-        slides[i].style['background-size'] =  'cover';
+        slides[i].style['background-size'] = 'cover';
         slides[i].style['background-position'] = 'center';
     }
 })();
+
+var menuButtonArea = document.querySelector('.menu-button-area');
+var menuButton = document.querySelector('.menu-btn');
+var menu = document.querySelector('.header');
+menuButtonArea.addEventListener('click', function () {
+    menu.classList.toggle('header-active');
+    menuButton.classList.toggle('menu-btn-active');
+});
